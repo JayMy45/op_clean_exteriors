66cd1dc1fecd1c774653d3f720c805629003050e
Jeremy Myers <echelonical@gmail.com>
2023-08-02 18:08:14 -0500

    create routes for Home, About, Testimonial, and Contact pages

--- app/navbar.js
+++ app/navbar.js
@@ -8,17 +8,17 @@ export default function NavBar() {
             <div className="flex flex-row justify-between">
                 <div className="w-1/4 ">
                     <Image
-                        src="/OpClean.Transparent.png"
+                        src="/OpCln.ColorTrans.png"
                         width={200}
                         height={200}
-                        className="bg-sky-200 rounded-lg mx-auto my-2"
+                        className="bg-sky-100 rounded-xl shadow-lg mx-auto my-2"
                     />
                 </div>
                 <div className="flex justify-evenly items-center w-2/4">
                     <Link href="/">Home</Link>
-                    <Link href="/">About</Link>
-                    <Link href="/">Testimonial</Link>
-                    <Link href="/">Contact</Link>
+                    <Link href="about">About</Link>
+                    <Link href="testimonial">Testimonial</Link>
+                    <Link href="contact">Contact</Link>
                 </div>
                 <div className="w-1/4">
                     <div></div>
---
+++ app/testimonial/page.js
@@ -0,0 +1,8 @@
+export default function TestimonialPage() {
+
+    return (
+        <>
+            <h1>Hellow Testimonial Pagie</h1>
+        </>
+    )
+}
