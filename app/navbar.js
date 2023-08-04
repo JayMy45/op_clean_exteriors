503f7f2834feeafd36567b589cf5914ab87a8811
Jeremy Myers <echelonical@gmail.com>
2023-08-04 17:38:45 -0500

    hamburger menu functional

--- app/navbar.js
+++ app/navbar.js
@@ -2,8 +2,8 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { AiOutlineClose, AiOutlineFacebook, AiOutlineInstagram, AiOutlineMenu, AiOutlineTwitter } from "react-icons/ai";
 
 export default function NavBar() {
 
@@ -12,6 +12,11 @@ export default function NavBar() {
     const handleToggle = () => {
         setNavbarOpen(!navbarOpen);
     }
+
+    useEffect(() => {
+        console.log('NavBar rendered');
+    }, []);
+
 
     return (
         <>
@@ -31,10 +36,11 @@ export default function NavBar() {
                     <div className="flex items-center justify-center w-full h-full border">
                         <div className="flex w-full border">
                             <div className="mx-auto space-x-5">
-                                <Link href="/">Home</Link>
-                                <Link href="about">About</Link>
-                                <Link href="testimonial">Testimonial</Link>
-                                <Link href="contact">Contact</Link>
+                                <Link href="/" className="uppercase">Home</Link>
+                                <Link href="about" className="uppercase">About</Link>
+                                <Link href="testimonial" className="uppercase">Testimonial</Link>
+                                <Link href="contact" className="uppercase">Services</Link>
+                                <Link href="contact" className="uppercase">Contact</Link>
                             </div>
                         </div>
                     </div>
@@ -50,6 +56,76 @@ export default function NavBar() {
                         size={30}
                         className=""
                     />
+                </div>
+                <div className={navbarOpen
+                    ? "fixed left-0 top-0 w-[75%] h-screen bg-slate-800 p-7 ease-in duration-500"
+                    : "fixed left-[-100%]  w-[75%] h-screen top-0 p-10 ease-in duration-500"
+                }
+                >
+                    <div className="flex w-full items-center justify-end">
+                        <div onClick={handleToggle} className="cursor-pointer">
+                            <AiOutlineClose
+                                size={30}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col py-4">
+                        <ul>
+                            <Link href="/">
+                                <li
+                                    onClick={() => setNavbarOpen(false)}
+                                    className="py-4 cursor-pointer">
+                                    Home
+                                </li>
+                            </Link>
+                            <Link href="about">
+                                <li
+                                    onClick={() => setNavbarOpen(false)}
+                                    className="py-4 cursor-pointer">
+                                    About
+                                </li>
+                            </Link>
+                            <Link href="testimonial">
+                                <li
+                                    onClick={() => setNavbarOpen(false)}
+                                    className="py-4 cursor-pointer">
+                                    Testimonial
+                                </li>
+                            </Link>
+                            <Link href="contact">
+                                <li
+                                    onClick={() => setNavbarOpen(false)
+                                    }
+                                    className="py-4 cursor-pointer">
+                                    Contact
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                    <div>
+                        <div className="flex flex-row justify-around pt-10 items-center">
+
+                            <AiOutlineInstagram
+                                size={30}
+                                className="cursor-pointer"
+                                onClick={() => setNavbarOpen(false)
+                                }
+                            />
+                            <AiOutlineFacebook
+                                size={30}
+                                className="cursor-pointer"
+                                onClick={() => setNavbarOpen(false)
+                                }
+                            />
+                            <AiOutlineTwitter
+                                size={30}
+                                className="cursor-pointer"
+                                onClick={() => setNavbarOpen(false)
+                                }
+                            />
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
