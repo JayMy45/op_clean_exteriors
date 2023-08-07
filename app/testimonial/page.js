3caf0010358abf892217f7e93be147e07c0ee668
Jeremy Myers <echelonical@gmail.com>
2023-08-07 16:36:22 -0500

    Comments added to show where different parts of card are for readability

--- app/testimonial/page.js
+++ app/testimonial/page.js
@@ -9,21 +9,18 @@ export default function TestimonialPage() {
                     <h1>Work</h1>
                 </div>
             </div>
+
+            {/* Main div */}
             <div className="h-fit px-2 md:px-28 border">
                 <div className="text-center p-10 border">
                     <h2>Previous Work</h2>
                 </div>
-                {/* Before/After Card Container */}
+                {/* Before and After Card Container */}
                 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-9 py-5 border">
-                    {/* <section className="flex py-5 border"> */}
 
-                    {/* Siding Bfr/Aftr Card */}
-                    {/* <div className="flex flex-col md:flex-row  md: space-x-24 space-y-5 md:space-y-0 mx-auto"> */}
-
+                    {/* Siding Card */}
                     <div className="shadow-xl w-fit rounded-md overflow-hidden pb-2 mx-auto bg-slate-500">
-                        <div>
-                            <h3></h3>
-                        </div>
+
                         <div className="flex flex-row  space-x-2 bg-white justify-center items-center h-auto">
                             <div className="flex flex-row  space-x-2 bg-white h-96">
                                 <div className="flex flex-col h-96 bg-zinc-400">
@@ -31,6 +28,7 @@ export default function TestimonialPage() {
                                         <h3 className="text-black text-center font-semibold text-xl p-2">Before</h3>
                                     </div>
 
+                                    {/* Siding Before */}
                                     <div className="flex-grow flex items-center justify-center">
                                         <Image
                                             src={"https://res.cloudinary.com/dp04hh5pz/image/upload/v1691371604/OpClean/beforeAfter/SidingBefore_x9c4mk.png"}
@@ -40,6 +38,8 @@ export default function TestimonialPage() {
                                         />
                                     </div>
                                 </div>
+
+                                {/* Siding After */}
                                 <div className="flex flex-col h-96 bg-slate-300">
                                     <div>
                                         <h3 className="text-black text-center font-semibold text-xl p-2">After</h3>
@@ -55,23 +55,24 @@ export default function TestimonialPage() {
                                 </div>
                             </div>
                         </div>
+
+                        {/* Siding  Bottom*/}
                         <div className="p-4 bg-slate-500">
                             <p className="text-center font-bold text-2xl">Siding</p>
                         </div>
                     </div>
 
-                    {/* Concrete Bfr/Aftr Card */}
+                    {/* Concrete Card */}
                     <div className="shadow-xl w-fit rounded-md overflow-hidden pb-2 mx-auto bg-slate-500">
-                        <div>
-                            <h3></h3>
-                        </div>
+
                         <div className="flex flex-row  space-x-2 bg-white justify-center items-center h-auto">
+
                             {/* Concrete Before */}
                             <div className="flex flex-col h-96  bg-zinc-400">
                                 <div>
                                     <h3 className="text-black text-center font-semibold text-xl p-2">Before</h3>
                                 </div>
-                                <div className="flex flex-col justify-evenly">
+                                <div className="">
                                     <div className="pb-1">
                                         <Image
                                             src={"https://res.cloudinary.com/dp04hh5pz/image/upload/v1691442785/OpClean/beforeAfter/Before_and_AfterUpper_y8ax7m.jpg"}
@@ -91,6 +92,7 @@ export default function TestimonialPage() {
                                     </div>
                                 </div>
                             </div>
+
                             {/* Concrete After */}
                             <div className="flex flex-col h-96  bg-slate-300">
                                 <div>
@@ -117,17 +119,17 @@ export default function TestimonialPage() {
                                 </div>
                             </div>
                         </div>
+
+                        {/* Concrete Bottom */}
                         <div className="p-4 bg-slate-500">
                             <p className="text-center font-bold text-2xl">Concrete</p>
                         </div>
                     </div>
 
-                    {/* Building Bfr/Aftr Card */}
+                    {/* Brick Building Card */}
                     <div className="shadow-xl w-fit rounded-md overflow-hidden pb-2 mx-auto bg-slate-500">
-                        <div>
-                            <h3></h3>
-                        </div>
                         <div className="flex flex-row  space-x-2 bg-white justify-center items-center h-auto">
+
                             {/* Building Before */}
                             <div className="flex flex-col h-96  bg-zinc-400">
                                 <div>
@@ -158,18 +160,18 @@ export default function TestimonialPage() {
                                 </div>
                             </div>
                         </div>
+
+                        {/* Brick Bottom */}
                         <div className="p-4 bg-slate-500">
                             <p className="text-center font-bold text-2xl">Building</p>
                         </div>
                     </div>
 
-                    {/* Building Bfr/Aftr Card */}
+                    {/* Building Sidewalk Card */}
                     <div className="shadow-xl w-fit rounded-md overflow-hidden pb-2 mx-auto bg-slate-500">
-                        <div>
-                            <h3></h3>
-                        </div>
+
                         <div className="flex flex-row  space-x-2 bg-white justify-center items-center h-auto">
-                            {/* Building Before */}
+                            {/* bSidewalk Before */}
                             <div className="flex flex-col h-96  bg-zinc-400">
                                 <div>
                                     <h3 className="text-black text-center font-semibold text-xl p-2">Before</h3>
@@ -184,7 +186,7 @@ export default function TestimonialPage() {
                                 </div>
                             </div>
 
-                            {/* Building After */}
+                            {/* bSidewalk After */}
                             <div className="flex flex-col h-96  bg-slate-300">
                                 <div>
                                     <h3 className="text-black text-center font-semibold text-xl p-2">After</h3>
@@ -199,6 +201,8 @@ export default function TestimonialPage() {
                                 </div>
                             </div>
                         </div>
+
+                        {/* bSidewalk Bottom */}
                         <div className="p-4 bg-slate-500">
                             <p className="text-center font-bold text-2xl">SideWalks</p>
                         </div>
