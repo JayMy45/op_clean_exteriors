4059969755068e5cd0e1d50bb93aace0a2eb11d9
Jeremy Myers <echelonical@gmail.com>
2023-08-11 16:23:46 -0500

    updated form layout further

--- app/contact/form.js
+++ app/contact/form.js
@@ -1,12 +1,12 @@
 export default function ContactForm() {
     return (
         <>
-            <form className="w-max md:w-1/2 bg-blend-lighten h-full p-0 md:p-10">
+            <form className="w-screen md:w-1/2 bg-blend-lighten h-full p-0 md:p-10">
                 <div className="p-2 rounded-md shadow-lg bg-sky-900">
                     <div className="items-center rounded-md md:bg-slate-700 p-2">
 
                         {/* Name */}
-                        <div className="p-1 mb-3 border border-slate-400 rounded-md">
+                        <div className="p-1 mb-3 border border-slate-400 rounded-md w-2/3 mx-auto md:w-full">
                             <input
                                 className="text-black p-1 w-full"
                                 placeholder="Full Name"
@@ -15,7 +15,7 @@ export default function ContactForm() {
                         </div>
 
                         {/* Phone */}
-                        <div className="p-1 mb-3 mb-2 border border-slate-400 rounded-md">
+                        <div className="p-1 mb-3 mb-2 border border-slate-400 rounded-md w-2/3 mx-auto md:w-full">
                             <input
                                 className="text-black p-1 w-full"
                                 placeholder="Phone Number"
@@ -24,7 +24,7 @@ export default function ContactForm() {
                         </div>
 
                         {/* Email */}
-                        <div className="p-1 mb-3 border border-slate-400 rounded-md">
+                        <div className="p-1 mb-3 border border-slate-400 rounded-md  w-2/3 mx-auto md:w-full">
                             <input
                                 className="text-black p-1 w-full"
                                 placeholder="Email Address"
@@ -33,7 +33,7 @@ export default function ContactForm() {
                         </div>
 
                         {/* Message */}
-                        <div className="p-1 border border-slate-400 rounded-md">
+                        <div className="p-1 border border-slate-400 rounded-md w-2/3 mx-auto md:w-full">
                             <textarea
                                 id="message"
                                 placeholder="Leave a message..."
@@ -41,12 +41,12 @@ export default function ContactForm() {
                                 rows="4"
                                 minLength={10}
                                 maxLength={500}
-                                className="text-black p-1 w-full"
+                                className="text-black  w-full"
                             />
                         </div>
                     </div>
-                    <div className="p-1">
-                        <button className="bg-slate-500 rounded-md p-1 shadow-lg">
+                    <div className="p-1 flex justify-center">
+                        <button className="bg-slate-500 rounded-md p-1 shadow-lg  w-2/3 md:w-full">
                             Submit
                         </button>
                     </div>
