dfa23a73adf2cf6603ac5491cc8bd90ee1acce57
Jeremy Myers <echelonical@gmail.com>
2023-08-15 12:19:44 -0500

    update to layout of video on smaller screens

--- app/page.js
+++ app/page.js
@@ -2,17 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-hidden">
 
       {/* Nashville Video */}
-      <video
-        id='fullscreenVideo'
-        autoPlay
-        muted
-        className="absolute w-screen h-screen object-cover"
-      >
-        <source src="https://res.cloudinary.com/dp04hh5pz/video/upload/v1692109540/OpClean/Vidoes/Nashville.Video_mtqhuc.mp4" type="video/mp4" />
-      </video>
+      <div class="video-container">
+        <video
+          id='fullscreenVideo'
+          autoPlay
+          muted
+          className="w-screen h-screen object-cover flex-1"
+        >
+          <source src="https://res.cloudinary.com/dp04hh5pz/video/upload/v1692109540/OpClean/Vidoes/Nashville.Video_mtqhuc.mp4" type="video/mp4" />
+        </video>
+      </div>
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed md:hidden bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
