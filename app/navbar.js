96348a4dff802e036aaeac49d2bd950f2e072c40
Jeremy Myers <echelonical@gmail.com>
2023-08-18 01:02:13 -0500

    format updates

--- app/navbar.js
+++ app/navbar.js
@@ -7,6 +7,7 @@ export default function NavBar() {
     return (
         <>
             <div className="flex flex-row justify-end md:justify-between border-b md:border-0 bg-slate-500 bg-opacity-50 md:bg-transparent">
+
                 <div className="hidden md:block w-1/2 md:m-2">
                     <div className="flex justify-left">
                         <Link href="/" className="uppercase">
@@ -25,19 +26,18 @@ export default function NavBar() {
                 <div className="hidden md:block w-1/2 h-inherent">
                     <div className="flex items-center justify-center w-full h-full">
                         <div className="flex w-full">
-                            <div className="ml-auto pr-10 space-x-5 text-orange-700">
-                                <Link href="/" className="uppercase">Home</Link>
-                                <Link href="about" className="uppercase">About</Link>
-                                <Link href="work" className="uppercase">Work</Link>
-                                <Link href="services" className="uppercase">Services</Link>
-                                <Link href="contact" className="uppercase">Contact</Link>
+                            <div className="ml-auto pr-10 space-x-5 text-green-800">
+                                <Link href="/" className="uppercase hover:text-amber-600">Home</Link>
+                                <Link href="about" className="uppercase hover:text-amber-600">About</Link>
+                                <Link href="work" className="uppercase hover:text-amber-600">Work</Link>
+                                <Link href="services" className="uppercase hover:text-amber-600">Services</Link>
+                                <Link href="contact" className="uppercase hover:text-amber-600">Contact</Link>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
                 <Hamburger />
+
             </div>
         </>
     )
