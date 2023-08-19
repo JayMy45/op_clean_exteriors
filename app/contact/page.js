b40af30b7eecbc3fc00b31974339e15158356911
Jeremy Myers <echelonical@gmail.com>
2023-08-19 17:39:18 -0500

    update mockup for service and aboutme pages. Also update sizing of Contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,35 +4,39 @@ import ContactForm from "./form";
 export default function ContactPage() {
     return (
         <>
-            <div className="container-page text-center mt-5">
-                <h1 className="uppercase font-bold text-2xl md:text-4xl">Contact Me</h1>
-            </div>
-
-            {/* Form Container */}
-            <div className="h-full md:p-5 flex items-center justify-center mt-5">
-                <div className="flex md:rounded-lg shadow-2xl overflow-hidden bg-gray-900 bg-opacity-25">
-
-                    <ContactForm className="flex-grow" />
-
-                    <div className="h-full hidden lg:block">
-                        <Image
-                            src="https://res.cloudinary.com/dp04hh5pz/image/upload/v1691463621/OpClean/beforeAfter/OpCleanGear_uoam4j.png"
-                            width={816}
-                            height={816}
-                            alt="OpClean Gear"
-                        />
-                    </div>
-                    <div className="flex justify-center items-center my-auto mr-3 rounded-2xl border border-green-600 bg-opacity-70 h-full hidden md:block bg-zinc-600 lg:hidden">
-                        <Image
-                            src="/OpCln.ColorTrans.png"
-                            width={500}
-                            height={500}
-                            alt="OpClean Gear"
-                        />
-                    </div>
-
+            <div className="h-screen md:h-fit md:px-28">
+                <div className="text-center p-10">
+                    {/* <div className="container-page text-center"> */}
+                    <h1 className="uppercase font-bold text-2xl md:text-4xl">Contact Me</h1>
                 </div>
-            </div>
+
+                {/* Form Container */}
+                <div className=" md:p-5 flex items-center justify-center">
+                    <div className="flex md:rounded-lg shadow-2xl overflow-hidden bg-gray-900 bg-opacity-25">
+
+                        <ContactForm className="flex-grow" />
+
+                        <div className="h-full hidden lg:block">
+                            <Image
+                                src="https://res.cloudinary.com/dp04hh5pz/image/upload/v1691463621/OpClean/beforeAfter/OpCleanGear_uoam4j.png"
+                                width={816}
+                                height={816}
+                                alt="OpClean Gear"
+                            />
+                        </div>
+                        <div className="flex justify-center items-center my-auto mr-3 rounded-2xl border border-green-600 bg-opacity-70 h-full hidden md:block bg-zinc-600 lg:hidden">
+                            <Image
+                                src="/OpCln.ColorTrans.png"
+                                width={250}
+                                height={250}
+                                alt="OpClean Gear"
+                            />
+                            {/* </div> */}
+
+                        </div>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
--- app/services/page.js
+++ app/services/page.js
@@ -2,11 +2,17 @@ export default function Services() {
 
     return (
         <>
-            <div>
-                Services anybody?
-            </div>
-            <div className="h-screen text-center">
-                <h1>Services</h1>
+
+            <div className="h-fit px-2 md:px-28">
+                <div className="text-center p-10">
+                    <h1 className="text-4xl font-bold uppercase">Services</h1>
+                    <div className="h-screen">
+                        <div className="mt-5 mx-auto h-screen  border border-black rounded-xl bg-slate-200 bg-opacity-50 max-w-5xl">
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
